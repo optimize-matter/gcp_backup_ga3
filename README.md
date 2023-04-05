cfe8c11281c5ccb355cb5968fc1327e1437d632c
Maxime Baron <maxime.baron2002@gmail.com>
2023-04-05 16:22:11 +0200

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# global.py
+Permets de faire des backups sur les dimensions et metrics de son choix. Se référer au [dev tools](https://ga-dev-tools.web.app/dimensions-metrics-explorer/) pour connaître la compatibilité des metrics et dimensions entre eux. 
+
+## Setup 
+Copier le script et le requirement dans une cloud function ayant **512MB de mémoire**. Ajoutere le crédential sur le GA avec le role viewer. Faire une requête POST avec un body, voir la section "Params"
+
+## Features
+
+✅ Vérification de l'existence du dataset et de la table  
+✅ Création automatique du dataset et de la table  
+✅ Création automatique de schéma de données   
+✅ Sélection des dims et métriques désiré   
+✅ Possibilité de reprendre une backup arrêté à un pageToken ou à une date (manuellement)  
+✅ Prise en compte des customs Dimentions   
+
+## Params  
+### REQUIRE :  
+**viewId :** Id de vue GA 
+**projectId :** ProjectId GCP
+**datasetId :** Nom du dataset
+**tableId :** Nom de la table
+**dimensions :** list contenant le nom des dimensions mais sans le "ga:" ⚠ Pas plus de **9** dimensions
+*Exemple :*
+```json
+"dimensions":[
+        {"dimension":"ga:sourceMedium","column":"source_medium"},
+        {"dimension":"ga:deviceCategory","column":"device_type"},
+        {"dimension":"ga:date","column":"date"}
+],
+```
+**metrics :** list contenant le nom des metrics mais sans le "ga:"  ⚠ Pas plus de **10** metrics
+*Exemple :*
+```json
+"metrics":[
+    {"metric":"ga:users","column":"users"},
+    {"metric":"ga:newUsers","column":"new_users"}
+]
+```
+### OPTIONNAL :
+**clusteringFields :** list des champs pour cluster la table BQ ('view_id' et 'country' par défaut)  
+**startDate :** string (YYYY-MM-DD, 2005-01-01 par défaut)  
+**endDate :** string (YYYY-MM-DD, "today" par défaut)  
+**pageToken :** string (en cas d'arrêt prématurer écrire le pageToken qui était en cours de traitement)
+
---
+++ global.py
@@ -0,0 +1,307 @@
+from googleapiclient.discovery import build
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+from google.oauth2 import service_account
+from datetime import datetime
+import pandas as pd
+
+CREDENTIALS = service_account.Credentials.from_service_account_file('key.json')
+
+"""Initialisation de l'API Managemet analytics"""
+def initialize_analyticsManagement(credentials):
+  # Build the service object.
+  return build('analytics', 'v3', credentials=credentials)
+"""Initialisation de l'API Matadata analytics"""
+def initialize_analyticsMetadata(credentials):
+  # Build the service object.
+  return build('analytics', 'v3', credentials=credentials)
+"""Initialisation de l'API Reporting Analytics"""
+def initialize_analyticsreporting(credentials):
+  # Build the service object.
+  return build('analyticsreporting', 'v4', credentials=credentials)
+"""Initialisation de l'API BQ"""
+def initialize_bigquery(credentials,project_id):
+    return bigquery.Client(credentials= credentials,project=project_id)
+
+def formatDimMet(dimensions,metrics,service):
+    columns = service.metadata().columns().list(reportType='ga').execute()
+
+     # Récupération des id de tout les metrics et dims
+    dimensionsId=[dimension['dimension'] for dimension in dimensions]
+    metricsId=[metric['metric'] for metric in metrics]
+
+    # Si le tableau les metrics et dimensions existe dans ga on les récupére
+    def checkDimMet(item):
+        if item['id'] in dimensionsId or item['id'] in metricsId:
+            return True
+        return False
+
+    dims = filter(checkDimMet,columns['items'])
+    dims = list(dims)
+    dims = [{'id':dims[i]['id'],'type':dims[i]['attributes']['dataType']} for i in range(len(dims))]#On fais un dico avec l'id et le type du met ou dims
+    return dims
+
+def formatCustomDimMet(dimensions,metrics,service,accountId,webPropertyId):
+    # Récupération des id de tout les metrics et dims du compte GA
+    dimensionsCustom = service.management().customDimensions().list(accountId=accountId,webPropertyId=webPropertyId).execute()
+    metricsCustom = service.management().customMetrics().list(accountId=accountId,webPropertyId=webPropertyId).execute()
+
+    # Récupération des id de tout les metrics et dims du body
+    dimensionsId=[dimension['dimension'] for dimension in dimensions]
+    metricsId=[metric['metric'] for metric in metrics]
+
+    # Si le tableau les metrics et dimensions existe dans ga on les récupére
+    def checkDim(item):
+        if item['id'] in dimensionsId:
+            return True
+        return False
+
+    def checkMet(item):
+        if item['id'] in metricsId:
+            return True
+        return False
+    
+    dims = list(filter(checkDim,dimensionsCustom['items']))
+    dims = [{'id':dims[i]['id'],'type':'STRING'} for i in range(len(dims))]#On fais un dico avec l'id et le type du met ou dims
+
+    met = list(filter(checkMet,metricsCustom['items']))
+    met = [{'id':met[i]['id'],'type':met[i]['type']} for i in range(len(met))]#On fais un dico avec l'id et le type du met ou dims
+    return dims+met
+
+def createSchema(dimensions,metrics,dims):
+    schema = []
+
+    #Tout ça pour ajouter dans le dico le nom des colonnes tel qu'on les veux 
+    for dim in dims:
+        for dimension in dimensions:
+            if dim['id'] == dimension['dimension']:
+                dim['column'] = dimension['column']
+        for metric in metrics:
+            if dim['id'] == metric['metric']:
+                dim['column'] = metric['column']
+
+    # On crée les colonnes bigquery avec le bon nom et le bon Type !
+    for dim in dims:
+        if dim['id'] == 'ga:date':
+            schema.append(bigquery.SchemaField(dim['column'],"DATE",mode="NULLABLE"))
+        elif dim['type'] == 'INTEGER':
+            schema.append(bigquery.SchemaField(dim['column'],"INTEGER",mode="NULLABLE"))
+        elif dim['type'] == 'FLOAT':
+            schema.append(bigquery.SchemaField(dim['column'],"FLOAT",mode="NULLABLE"))
+        elif dim['type'] == 'PERCENT':
+            schema.append(bigquery.SchemaField(dim['column'],"FLOAT",mode="NULLABLE"))
+        elif dim['type'] == 'TIME':
+            schema.append(bigquery.SchemaField(dim['column'],"TIME",mode="NULLABLE"))
+        elif dim['type'] == 'CURRENCY':
+            schema.append(bigquery.SchemaField(dim['column'],"FLOAT",mode="NULLABLE"))
+        else:
+            schema.append(bigquery.SchemaField(dim['column'],"STRING",mode="NULLABLE"))
+    schema.append(bigquery.SchemaField("view_id","STRING",mode="NULLABLE"))
+    schema.append(bigquery.SchemaField("country","STRING",mode="NULLABLE"))
+    return schema
+
+def exist_dataset_table(client, table_id, dataset_id, project_id,clusteringFields,dimensions,schema):
+    print('check_dataset')
+
+    try:
+        dataset_ref = "{}.{}".format(project_id, dataset_id)
+        client.get_dataset(dataset_ref)  # Make an API request.
+        print("data set id présent")
+    except NotFound:
+        dataset_ref = "{}.{}".format(project_id, dataset_id)
+        dataset = bigquery.Dataset(dataset_ref)
+        dataset.location = "europe-west1"
+        dataset = client.create_dataset(dataset)  # Make an API request.
+
+    try:
+        table_ref = "{}.{}.{}".format(project_id, dataset_id, table_id)
+        client.get_table(table_ref)  # Make an API request.
+    except NotFound:
+        table_ref = "{}.{}.{}".format(project_id, dataset_id, table_id)
+        table = bigquery.Table(table_ref, schema=schema)
+        if 'date' in dimensions:
+            table.time_partitioning = bigquery.TimePartitioning(
+                type_=bigquery.TimePartitioningType.DAY,
+                field="date"
+            )
+        if clusteringFields is not None:
+            table.clustering_fields = clusteringFields
+
+        table = client.create_table(table)  # Make an API request.
+
+    return 'ok'
+
+def constructBody(VIEW_ID,startDate,endDate,dimensionLabel,metricsLabel,pagetoken=None):
+    
+    dimId=[{"name":dimension['dimension']} for dimension in dimensionLabel]
+    metId=[{"expression":metric['metric']} for metric in metricsLabel]
+    body= {'reportRequests': 
+                [
+                    {'viewId': VIEW_ID,
+                    'dateRanges': [{'startDate': startDate, 'endDate': endDate}],
+                    'dimensions':dimId,
+                    'metrics':metId,
+                    'pageSize':"100000",
+                    'pageToken':pagetoken},
+                ],
+        }
+    return body
+
+def verifEchantillion(rsp):
+    for report in rsp.get('reports', []):
+        if report.get('data', {}).get('samplesReadCounts'):
+            return True
+        else:
+            return False
+
+def traitementDonnées(rsp,dimensionLabel,metricsLabel,view_id,country):
+    print("Traitement des données en cours...")
+    #Récupération des dimensions
+    rows = []
+    dimensionsName = rsp['reports'][0]['columnHeader']['dimensions']
+
+    dimensionsCol = [dimensionLabel[i]['column'] for i in range(len(dimensionLabel))]
+    metricsCol = [metricsLabel[i]['column'] for i in range(len(metricsLabel))]
+
+    #Récupération des metrics
+    metricsName = []
+    for metricsHeader in rsp['reports'][0]['columnHeader']['metricHeader']['metricHeaderEntries']:
+        metricsName.append(metricsHeader['name'])
+
+    # Transformation des données en list de dico, peux importe l'ordre des dimensions quand on les à défini
+    for r in rsp['reports'][0]['data']['rows']:
+        row = {}
+        dimensionsValue = r['dimensions']
+        metricsValue = r['metrics'][0]['values']
+        for i in range(len(dimensionsValue)):
+            if dimensionsName[i] == 'ga:date':
+                date = datetime.strptime(dimensionsValue[i],"%Y%m%d").date()
+                row.update({dimensionsName[i]:date})
+            else:
+                row.update({dimensionsName[i]:dimensionsValue[i]})
+        for i in range(len(metricsValue)):
+            row.update({metricsName[i]:metricsValue[i]})
+        rows.append(row)
+
+    # Transformation de la list en dataFrame
+    rows = pd.DataFrame(rows)
+
+    # Transformation des types de données pour matcher avec BQ
+    metrics = rsp['reports'][0]['columnHeader']['metricHeader']['metricHeaderEntries']
+    for met in metrics:
+        if met['type'] == "INTEGER":
+            rows = rows.astype({met['name']:int})
+        elif met['type'] == "FLOAT":
+            rows = rows.astype({met['name']:float})
+        elif met['type'] == "CURRENCY":
+            rows = rows.astype({met['name']:float})
+        elif met['type'] == "PERCENT":
+            rows = rows.astype({met['name']:float})
+        elif met['type'] == "TIME":
+            rows = rows.astype({met['name']:datetime.time()})
+
+    # Renomage des colonnes (pour retirer le ":ga")
+    rows.columns = dimensionsCol+metricsCol
+    rows = rows.assign(view_id=[view_id]*len(rows))
+    rows = rows.assign(country=[country]*len(rows))
+    return rows
+
+def addToBQ(bq,PROJECT_ID,DATASET_ID,TABLE_ID,data,dimensionLabel):
+    print("Ajout des données à BQ en cours...")
+    table_ref = "{}.{}.{}".format(PROJECT_ID, DATASET_ID, TABLE_ID)
+    table = bq.get_table(table_ref)
+    write = 'WRITE_APPEND'
+
+    dimensionsCol = [dimensionLabel[i]['column'] for i in range(len(dimensionLabel))]
+
+    if 'ga:date' in dimensionsCol:
+        job_config = bigquery.LoadJobConfig(
+            # Si la colonne ga:date est présente, le code définit le type de données de la colonne comme étant de type DATE dans le schéma de la table
+            schema = [bigquery.SchemaField("date", bigquery.enums.SqlTypeNames.DATE)],
+            write_disposition = write,
+        )
+    else:
+        job_config = bigquery.LoadJobConfig(
+            write_disposition = write,
+        )
+        
+    load_job = bq.load_table_from_dataframe(data, table, job_config=job_config)
+    print(load_job.result())
+    print("Ajout terminé")
+
+def verifPageToken(rsp):
+    if 'nextPageToken' in rsp['reports'][0]:
+        return rsp['reports'][0]['nextPageToken']
+    else :
+        return None
+
+def verifOptionRequest(req):
+    if 'clusteringFields' in req:
+        clusteringFields = req['clusteringFields']
+    else:
+        clusteringFields = ['view_id','country']
+
+    if 'pageToken' in req:
+        pageToken = req['pageToken']
+    else:
+        pageToken = None
+    
+    if 'startDate' in req:
+        startDate = req['startDate']
+    else:
+        startDate = "2005-01-01"
+
+    if 'endDate' in req:
+        endDate = req['endDate']
+    else:
+        endDate = "today"
+    return clusteringFields,pageToken,startDate,endDate,
+
+def verifRequireRequest(req):
+    if 'country' in req and 'viewId' in req and 'projectId' in req and 'datasetId' in req and 'tableId' in req and 'dimensions' in req and 'metrics' in req:
+        return 'ok'
+    else:
+        return 'ko'
+
+def main(req):
+    req = req.get_json()# Récupération des paramétres du body
+    """Vérification des différent paramétre du body"""
+    if verifRequireRequest(req) != 'ok':
+        return 'Erreur de paramétres'
+    clusteringFields,pageToken,startDate,endDate = verifOptionRequest(req)#Vérification et définition des paramétre optionnel
+
+    """Initialisation des API"""
+    management = initialize_analyticsManagement(CREDENTIALS)# Initialisation de l'API Management
+    metadata = initialize_analyticsMetadata(CREDENTIALS)# Initialisation de l'API MetaData
+    analytics = initialize_analyticsreporting(CREDENTIALS)# Initialisation de l'API GA
+    bq = initialize_bigquery(CREDENTIALS, req['projectId'])# Initialisation de BQ
+
+    """Mise en forme des dimensions et metrics"""
+    allFormatedDimsAndMets = formatDimMet(req['dimensions'],req['metrics'],metadata)
+    allFormatedDimsAndMets += formatCustomDimMet(req['dimensions'],req['metrics'],management,req['accountId'],req['webPropertyID'])
+    schema = createSchema(req['dimensions'],req['metrics'],allFormatedDimsAndMets)#Création du schema
+    db = exist_dataset_table(bq, req['tableId'], req['datasetId'], req['projectId'],clusteringFields,req['dimensions'],schema)#Vérification du dataset et de la table, si elles existent pas on les crée
+    nombreRequête = 0 #Pour compter le nombre de requêtes  
+    rowsCount = 0 #Pour connaître le nbr de ligne
+    if db =='ok':
+        while startDate != endDate:#Enfin de boucle on assigne la date de fin du body à startDate et dans la boucle si c'est pas échantillonné la date de fin du body ne change pas donc le résultat fini par être vrais
+            nombreRequête+=1
+            reportEndDate = endDate
+            print("Dates concernées :",startDate, reportEndDate) # Date de récupération des premiére données utile en cas d'erreur
+            body = constructBody(req['viewId'],startDate,endDate,req['dimensions'],req['metrics'],pageToken) # Construction du body avec les paramétres de la requéte
+            response = analytics.reports().batchGet(body=body).execute()# Execution de la requéte
+            if verifEchantillion(response):# Si le rslt est échantilloner 
+                report_end_date = datetime.strptime(startDate,"%Y-%m-%d")+(datetime.strptime(reportEndDate,"%Y-%m-%d")-datetime.strptime(startDate,"%Y-%m-%d"))/2 # Nouvelle date = nombre de jour entre les dates diviser par 2
+                report_end_date = report_end_date.strftime("%Y-%m-%d")
+                print("Résultat échantillioné, nouvelle date :")#on retourne au début du while
+            else:#Le rslt n'est pas échantillonné 
+                rowsCount+= response['reports'][0]['data']['rowCount']
+                print("Résultat non échantillonné")
+                data = traitementDonnées(response,req['dimensions'],req['metrics'],req['viewId'],req['country'])# Traitement des données (mise en dataFrame & changement des type de données)
+                addToBQ(bq,req['projectId'],req['datasetId'],req['tableId'],data,req['dimensions'])# Ajout du data frame dans BQ 
+                pageToken = verifPageToken(response)#On regarde si il y a un pageToken
+                if pageToken == None:#Si il n'y en a pas 
+                    print("Prochaine page :",pageToken)
+                    startDate = reportEndDate #On passe à la prochaine date 
+    print("L'opération est un succés, en",nombreRequête,"requête(s), félicitation !",rowsCount,"Lignes ont été ajouté ! Les données sont en sécurité, retour à la base soldat")
+    return 'ok'
